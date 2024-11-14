44e2546d2c36faf3e82094d5a14392e67de7c608
Karma_dev1 <sasha.zvaritch@gmail.com>
2024-11-14 18:01:26 +0200

    add exp handler

--- src/components/Register-Info.vue
+++ src/components/Register-Info.vue
@@ -23,6 +23,46 @@ export default {
     async load() {
       const JWT = localStorage.getItem('jwt_token');  // Получаем токен из localStorage
 
+       let headers = {
+        'Content-Type': 'application/json'
+        };
+
+        headers['Authorization'] = 'Bearer' + JWT
+        this.$http.get('data/persons', {headers})
+        .then(response => {
+          response.json().then(data =>{
+            console.log(data);
+            data.forEach(person => this.persons.push(person));
+          }
+        )
+      }, error => {
+      if(error.status === 401) {
+        alert('Помилка завантаження даних, \n спочатку треба авторизуватись');
+      } else {
+        alert('Невідома помилка, \n повторіть пізніше');
+      }
+
+      async nenoughrights() {
+        let headers = {
+        'Content-Type': 'application/json'
+        };
+
+        headers['Authorization'] = 'Bearer' + JWT
+        this.$http.delete('data/persons/' + this.person.id, {headers})
+        .then (response => {
+          this.persons.splice(this.persons.indexOf(this.person), 1);
+        }, error => {
+          if (error.status === 401) {
+            alert('Помилка видалення даних, \n спочатку треба авторизуватись');
+          } else if (error.status === 403) {
+            alert('Недостатньо прав');
+          } else {
+            alert('Невідома помилка,\n повторіть пізніше');
+          }
+          }
+        )
+      }
+
       if (!JWT) {
         console.error('JWT token is missing');
         return;
@@ -41,9 +81,12 @@ export default {
       } catch (error) {
         console.error('Error loading register data:', error);
       }
-    },
-  },
-};
+      },
+    )}
+    }
+}
+
+
 </script>
 
 
